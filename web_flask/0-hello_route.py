58282345d14f82bc1cae68724e1df47673896ee4
Joseph Omoruwou <josephomoruwou@gmail.com>
2024-07-29 00:56:06 +0100

    update: updated documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python3
-# This script starts a Flask web application
+"""
+This script starts a Flask web application.
+
+Modules:
+    0-hello_route contains the definition of the Flask app and its routes
+
+Functions:
+    hello_hbnb: Returns a greeting message.
+
+"""
 
 from flask import Flask
 
-# Create the Flask app (an instance of the Flask class)
+""" Create the Flask app (an instance of the Flask class) """
 app = Flask(__name__)
 
 
-# Define the route for the Default(root) URL ("/")
-@app.route('/', strict_slashes=False)
+@app.route('/', strict_slashes=False)  # route decorator
 def hello_hbnb():
-    """ Displays 'Hello HBNB!' """
+    """
+    Returns:
+        str: 'Hello HBNB!'
+    """
     return 'Hello HBNB!'
 
 
-# Setup to run only if this file is being run directly
 if __name__ == '__main__':
+    """
+    Run the Flask app (only if this file is being run directly).
+    """
     app.run(host='0.0.0.0', port=5000)
